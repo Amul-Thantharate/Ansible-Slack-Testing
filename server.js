48360d649c0e0e9fefa7de79dcaba3008ad1d945
Amul-Thantharate <amulthantharate@gmail.com>
2024-01-21 09:55:44 +0530

    Added a ReadMe file

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-console.log('Starting server...');
-
-var server = http.createServer(function (request, response) {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Hello World\n');
-}).listen(8080);
